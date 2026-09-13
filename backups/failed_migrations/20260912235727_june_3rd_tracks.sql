43f7f0db1130a9c14305230dc8464a52e5272a04
Jontarius Cooper <jontariuscooper@Jontariuss-MacBook-Pro.local>
2026-09-13 00:19:57 -0400

    999 Wellness Intelligence latest updates

---
+++ backups/failed_migrations/20260912235727_june_3rd_tracks.sql
@@ -0,0 +1,31 @@
+insert into public.music_tracks (
+  artist_name,
+  title,
+  featured_artist,
+  album_name,
+  release_type,
+  track_number,
+  duration_seconds,
+  audio_path,
+  artwork_path,
+  spotify_url,
+  explicit,
+  active,
+  featured
+)
+values
+('Duke Da Boss X','Dont Worry If You Gonna Pray','Prince X','June 3rd','album',1,184,'duke-da-boss-x/june-3rd/01-dont-worry-if-you-gonna-pray.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,true),
+('Duke Da Boss X','God Choose 999','Prince X','June 3rd','album',2,190,'duke-da-boss-x/june-3rd/02-god-chose-999.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Court Issues','Prince X','June 3rd','album',3,260,'duke-da-boss-x/june-3rd/03-court-issues.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Envy Family','Prince X','June 3rd','album',4,153,'duke-da-boss-x/june-3rd/04-envy-family.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','God Da Boss','Prince X','June 3rd','album',5,272,'duke-da-boss-x/june-3rd/05-god-da-boss.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Happens To You','Prince X','June 3rd','album',6,213,'duke-da-boss-x/june-3rd/06-happens-to-you.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Back Now','Prince X','June 3rd','album',7,137,'duke-da-boss-x/june-3rd/07-back-now.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Fuck 100 Stacks','Prince X','June 3rd','album',8,202,'duke-da-boss-x/june-3rd/08-fuck-100-stacks.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Out Da Way','Prince X','June 3rd','album',9,221,'duke-da-boss-x/june-3rd/09-out-da-way.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Order 322','Prince X','June 3rd','album',10,199,'duke-da-boss-x/june-3rd/10-order-322.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Fuck Travis County Courthouse','Prince X','June 3rd','album',11,127,'duke-da-boss-x/june-3rd/11-fuck-travis-county-courthouse.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Make Em Lay','Prince X','June 3rd','album',12,158,'duke-da-boss-x/june-3rd/12-make-em-lay.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Judge Dimples Cum Too Me','Prince X','June 3rd','album',13,188,'duke-da-boss-x/june-3rd/13-judge-dimples-cum-too-me.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false),
+('Duke Da Boss X','Tank On E','Noor X feat. Prince X','June 3rd','album',14,270,'duke-da-boss-x/june-3rd/14-tank-on-e.mp3','duke-da-boss-x/june-3rd/cover.jpg','https://open.spotify.com/album/1PhasTMtDQzple1BeEDXrs',true,true,false);
+
